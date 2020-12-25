004d5600298b0766031caffd1b464872a6c9ec05
Adriaan van Niekerk <asvn90@gmail.com>
2020-12-25 22:02:55 +0000

    Update function retweet_tweets_with_hastag to exclude tweets already re-tweeted

--- hh.py
+++ hh.py
@@ -8,23 +8,9 @@ CONSUMER_SECRET = keys['consumer_secret']
 ACCESS_TOKEN = keys['access_token']
 ACCESS_TOKEN_SECRET = keys['access_token_secret']
 
-FILE_NAME_FAV = 'last_seen_id.txt'
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
-
-def retrieve_last_seen_id(file_name):
-    f_read = open(file_name, 'r')
-    last_seen_id = int(f_read.read().strip())
-    f_read.close()
-    return last_seen_id
-
-def store_last_seen_id(last_seen_id,file_name):
-    f_write = open(file_name, 'w')
-    f_write.write(str(last_seen_id))
-    f_write.close()
-    return
 
 def retweet_tweets_with_hashtag(api, need_hashtags):
     if type(need_hashtags) is list:
@@ -37,9 +23,10 @@ def retweet_tweets_with_hashtag(api, need_hashtags):
                 need_hashtags = list(need_hashtags)
                 if set(hashtags) & set(need_hashtags):
                     if tweet.user.id != api.me().id:
-                        api.retweet(tweet.id)
-                        logger.info(f"Retweeted tweet from {tweet.user.name}")
-                        time.sleep(5)
+                        if not tweet.retweeted:
+                           api.retweet(tweet.id)
+                           logger.info(f"Retweeted tweet from {tweet.user.name}")
+                           time.sleep(5)
             except tweepy.TweepError:
                 logger.error("Error on retweet", exc_info=True)
     else:

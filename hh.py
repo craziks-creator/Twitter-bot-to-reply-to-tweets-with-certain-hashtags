fc6eb6e07a4b6494025a0025d7d6f6a12f58e199
Adriaan van Niekerk <asvn90@gmail.com>
2020-12-25 21:35:18 +0000

    Add while loop to check periodically for new tweets. Add function to update .txt file. Add function to filter for specific hastags.

--- hh.py
+++ hh.py
@@ -8,15 +8,47 @@ CONSUMER_SECRET = keys['consumer_secret']
 ACCESS_TOKEN = keys['access_token']
 ACCESS_TOKEN_SECRET = keys['access_token_secret']
 
+FILE_NAME_FAV = 'last_seen_id.txt'
+
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id,file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def retweet_tweets_with_hashtag(api, need_hashtags):
+    if type(need_hashtags) is list:
+        search_query = f"{need_hashtags} -filter:retweets"
+        tweets = api.search(q=search_query, lang ="en", tweet_mode='extended')
+        for tweet in tweets:
+            hashtags = [i['text'].lower() for i in tweet.__dict__['entities']['hashtags']]
+            try:
+                need_hashtags = [hashtag.strip('#') for hashtag in need_hashtags]
+                need_hashtags = list(need_hashtags)
+                if set(hashtags) & set(need_hashtags):
+                    if tweet.user.id != api.me().id:
+                        api.retweet(tweet.id)
+                        logger.info(f"Retweeted tweet from {tweet.user.name}")
+                        time.sleep(5)
+            except tweepy.TweepError:
+                logger.error("Error on retweet", exc_info=True)
+    else:
+        logger.error("Hashtag search terms needs to be of type list", exc_info=True) 
+        return
+
+while True:
+        retweet_tweets_with_hashtag(api, ["#mybot"])
+        logger.info("Waiting...")
+        time.sleep(30)    
  
-twt = api.search("#mybot",result_type="new",count=5) 
- 
-for s in twt:
-   print(s.id)
-   sn = s.user.screen_name
-   m = "@%s " % (sn)
-   api.update_status(status=m, in_reply_to_status_id = s.id)
-print("Done!!!")
+
